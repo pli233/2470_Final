6c58f073ce553c32832f0f1dafd50c3db79c95b4
Peiyuan Li <peiyuanli3627@gmail.com>
2024-12-08 00:49:58 -0500

    update for 12.8

--- affectnet_util.py
+++ affectnet_util.py
@@ -1,35 +1,37 @@
 from PIL import Image
-import torch
 from torch.utils.data import Dataset
-
 import os
 import pandas as pd
 import numpy as np
-
-from torchvision import transforms
-
 class AffectDataSet(Dataset):
-    def __init__(self, data_path, phase, affcls, transform=None):
+    def __init__(self, data_path, phase, affcls, transform=None, exclude_classes=None):
         self.phase = phase
         self.transform = transform
         self.data_path = data_path
         self.cls = affcls
+        self.exclude_classes = exclude_classes if exclude_classes else []
 
+        # 根据分类数加载对应文件
         if affcls == 7:
             if phase == 'train':
-                df = pd.read_csv(os.path.join(self.data_path, '7cls_train.txt'), sep=' ', header=None,names=['name','label'])
+                df = pd.read_csv(os.path.join(self.data_path, '7cls_train.txt'), sep=' ', header=None, names=['name', 'label'])
             else:
-                df = pd.read_csv(os.path.join(self.data_path, '7cls_val.txt'), sep=' ', header=None,names=['name','label'])
+                df = pd.read_csv(os.path.join(self.data_path, '7cls_val.txt'), sep=' ', header=None, names=['name', 'label'])
         else:
             if phase == 'train':
-                df = pd.read_csv(os.path.join(self.data_path, '8cls_train.txt'), sep=' ', header=None,names=['name','label'])
+                df = pd.read_csv(os.path.join(self.data_path, '8cls_train.txt'), sep=' ', header=None, names=['name', 'label'])
             else:
-                df = pd.read_csv(os.path.join(self.data_path, '8cls_val.txt'), sep=' ', header=None,names=['name','label'])
+                df = pd.read_csv(os.path.join(self.data_path, '8cls_val.txt'), sep=' ', header=None, names=['name', 'label'])
+
+        # 过滤需要排除的类别
+        if self.exclude_classes:
+            df = df[~df['label'].isin(self.exclude_classes)]
 
         file_names = df["name"]
         self.data = df
         self.label = self.data.loc[:, 'label'].values
 
+        # 获取样本分布
         _, self.sample_counts = np.unique(self.label, return_counts=True)
         print(f' distribution of {phase} samples: {self.sample_counts}')
 
@@ -58,6 +60,3 @@ class AffectDataSet(Dataset):
 
     def get_labels(self):
         return self.label
-
-if __name__ == "__main__":
-    train_dataset = AffectDataSet(data_path = "/CSCI2952X/datasets/affectnet",affcls=7, phase = 'train')
