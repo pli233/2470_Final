a13791da74d8c2953de35a0b79d9e028a31f7b8a
Peiyuan Li <peiyuanli3627@gmail.com>
2024-12-08 05:08:08 -0500

    update for 5 class

--- affectnet_util.py
+++ affectnet_util.py
@@ -3,47 +3,45 @@ from torch.utils.data import Dataset
 import os
 import pandas as pd
 import numpy as np
+
 class AffectDataSet(Dataset):
-    def __init__(self, data_path, phase, affcls, transform=None, exclude_classes=None):
-        self.phase = phase
+    def __init__(self, data_path, train=True, affcls=7, transform=None, exclude_classes=None):
+        self.train = train
         self.transform = transform
         self.data_path = data_path
         self.cls = affcls
         self.exclude_classes = exclude_classes if exclude_classes else []
 
-        # 根据分类数加载对应文件
-        if affcls == 7:
-            if phase == 'train':
-                df = pd.read_csv(os.path.join(self.data_path, '7cls_train.txt'), sep=' ', header=None, names=['name', 'label'])
-            else:
-                df = pd.read_csv(os.path.join(self.data_path, '7cls_val.txt'), sep=' ', header=None, names=['name', 'label'])
-        else:
-            if phase == 'train':
-                df = pd.read_csv(os.path.join(self.data_path, '8cls_train.txt'), sep=' ', header=None, names=['name', 'label'])
-            else:
-                df = pd.read_csv(os.path.join(self.data_path, '8cls_val.txt'), sep=' ', header=None, names=['name', 'label'])
+        # 根据分类数加载文件
+        file_name = '7cls_train.txt' if train else '7cls_val.txt'
+        df = pd.read_csv(os.path.join(self.data_path, file_name), sep=' ', header=None, names=['name', 'label'])
 
         # 过滤需要排除的类别
         if self.exclude_classes:
             df = df[~df['label'].isin(self.exclude_classes)]
 
+        # 获取文件名和标签
         file_names = df["name"]
         self.data = df
         self.label = self.data.loc[:, 'label'].values
 
-        # 获取样本分布
-        _, self.sample_counts = np.unique(self.label, return_counts=True)
-        print(f' distribution of {phase} samples: {self.sample_counts}')
+        # 调试：打印过滤前后的标签分布
+        print(f"Original label distribution: {np.unique(self.label, return_counts=True)}")
 
+        # 重新映射标签到连续范围
+        unique_labels = np.unique(self.label)
+        self.label_map = {old_label: new_label for new_label, old_label in enumerate(unique_labels)}
+        self.label = np.array([self.label_map[label] for label in self.label])
+
+        print(f"Label mapping: {self.label_map}")
+        print(f"Adjusted label distribution: {np.unique(self.label, return_counts=True)}")
+
+        # 获取样本路径
         self.file_paths = []
-        if phase == 'train':
-            for f in file_names:
-                path = os.path.join(self.data_path, 'train', f)
-                self.file_paths.append(path)
-        else:
-            for f in file_names:
-                path = os.path.join(self.data_path, 'valid', f)
-                self.file_paths.append(path)
+        subfolder = 'train' if train else 'valid'
+        for f in file_names:
+            path = os.path.join(self.data_path, subfolder, f)
+            self.file_paths.append(path)
 
     def __len__(self):
         return len(self.file_paths)
@@ -57,6 +55,3 @@ class AffectDataSet(Dataset):
             image = self.transform(image)
 
         return image, label
-
-    def get_labels(self):
-        return self.label
